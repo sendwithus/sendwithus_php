056a43da42f7872f8022881500ffee313c2bfb78
forkata <chris.todorov@gmail.com>
2013-03-26 10:47:52 +0000

    Updated php client to support v1_0 of swu api. Added simpletest cases.

---
+++ lib/Error.php
@@ -0,0 +1,30 @@
+<?php namespace sendwithus;
+/**
+ * Send With Us PHP Client
+ * @author sendwithus.com
+ */
+
+class API_Error extends \Exception
+{
+	public function __construct($message=null, $status=null, $body=null, $json=null)
+	{
+		parent::__construct($message);
+		$this->status = $status;
+		$this->body = $body;
+		$this->json = $json;
+	}
+
+	public function getStatus() {
+		return $this->status;
+	}
+
+	public function getBody() {
+		return $this->body;
+	}
+
+	public function getJson() {
+		return $this->json;
+	}
+}
+
+?>
