118f530bb9d28746d5604b4bd85725a8167addcc
Dylan Moore <moore.dylan.e@gmail.com>
2015-09-17 14:53:47 -0400

    Update version number
    In this version:
    - Better batch execution and cancellation.

--- lib/API.php
+++ lib/API.php
@@ -22,7 +22,7 @@ class API {
     protected $API_VERSION = '1';
     protected $API_HEADER_KEY = 'X-SWU-API-KEY';
     protected $API_HEADER_CLIENT = 'X-SWU-API-CLIENT';
-    protected $API_CLIENT_VERSION = "2.9.0";
+    protected $API_CLIENT_VERSION = "2.10.0";
     protected $API_CLIENT_STUB = "php-%s";
 
     protected $DEBUG = false;
